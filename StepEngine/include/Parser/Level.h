aacbbb1fff7faa650dfac84835be731458e868c5
Dulaj Disanayaka <vidura.me@gmail.com>
2025-02-04 20:05:17 +0100

    feat: added a basic TUI to show a level

--- StepEngine/include/Parser/Level.h
+++ StepEngine/include/Parser/Level.h
@@ -8,4 +8,10 @@ struct Level
     Grid grid;
     uint32_t grid_size;
     DIRECTION facing;
+
+
+    ITEM &operator()(uint32_t x, uint32_t y)
+    {
+        return grid[y * grid_size + x];
+    }
 };
